35181a417d274ab41c3f2dd8b68b762d873d52dd
Arna <arna12@ru.is>
2013-12-16 12:24:41 +0000

    Maven file structure and pom

---
+++ MathApp/src/main/java/is/ru/MathApp/App.java
@@ -0,0 +1,13 @@
+package is.ru.MathApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
